de878b500d92d4062aa3d4e31a5518b5afceec04
Anthony D. Dutoi <adutoi@tengri>
2025-05-13 22:19:45 -0700

    promoted CI infrastructure from QodeApplications to new fermion_field code

---
+++ qode/fermion_field/__init__.py
@@ -0,0 +1,22 @@
+#    (C) Copyright 2025 Anthony Dutoi
+# 
+#    This file is part of Qode.
+# 
+#    Qode is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+# 
+#    Qode is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with Qode.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from .CI_space_traits import CI_space_traits
+from . import configurations
+from . import field_op
+from . import field_op_ham
